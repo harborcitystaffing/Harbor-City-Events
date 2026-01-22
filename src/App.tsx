b693af8cc99bceecef75f5003a819c2ad2b5b5e9
Obaid Syed <obaidsayed159117@gmail.com>
2026-01-21 19:45:30 -0500

    Fix: case-sensitive import for Home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
+import Home from "@/pages/Home";
 
 function Router() {
   return (
